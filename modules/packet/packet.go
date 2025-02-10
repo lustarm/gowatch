66aa4cd2de68534d0ac74abc196f0397499bd54f
jeweler <valve.valve101@gmail.com>
2025-02-09 23:04:57 -0500

    add udp and tcp handlers for each packet

--- modules/packet/packet.go
+++ modules/packet/packet.go
@@ -1,13 +1,37 @@
 package packet
 
 import (
-    "fmt"
-    "github.com/google/gopacket"
-    // "github.com/google/gopacket/pcap"
+	"fmt"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	// "github.com/google/gopacket/pcap"
 )
 
 func HandlePacket(packet gopacket.Packet) error {
-    fmt.Println("Handling packet: ", packet.Metadata())
 
+    // ! tcp packet
+    if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+        fmt.Println("tcp packet intercepted: ", packet.Data())
+        handleTCP(&packet)
+    }
+
+    // ! udp packet
+    if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
+        fmt.Println("udp packet intercepted: ", packet.Data())
+        handleUDP(&packet)
+    }
+
+    return nil
+}
+
+// ! later
+
+func handleUDP(p *gopacket.Packet) error {
+
+    return nil
+}
+
+func handleTCP(p *gopacket.Packet) error {
     return nil
 }
